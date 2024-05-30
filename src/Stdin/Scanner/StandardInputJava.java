c6db72fd5c4787b939fe771c8c1b35fdc52969a7
Matteus-Barbosa <barbosamateus2004@gmail.com>
2024-05-30 13:17:07 -0300

    code: Criação do pacote Stdin.Scanner com código de estudos sobre Standard Input in Java.

---
+++ src/Stdin/Scanner/StandardInputJava.java
@@ -0,0 +1,25 @@
+package Stdin.Scanner;
+
+import java.util.Scanner; // Importando do pacote java.util a classe Scanner
+
+public class StandardInputJava {
+    public static void main(String[] args) {
+        // Criando o objeto scanner:
+        Scanner scanner = new Scanner(System.in);
+
+        // Coletando dados do usuário:
+        System.out.println("Digite o seu nome: ");
+        String nome = (scanner.next());
+        System.out.println("Digite seu Sobrenome: ");
+        String sobrenome = (scanner.next());
+        System.out.println("Digite a sua idade: ");
+        int idade = (scanner.nextInt());
+        System.out.println("Por fim, digite a sua altura: ");
+        double altura = (scanner.nextDouble());
+
+        // Imprimindo os resultados obtidos:
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+    }
+}
