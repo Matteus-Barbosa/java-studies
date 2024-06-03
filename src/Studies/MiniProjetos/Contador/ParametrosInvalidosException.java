57a27b4d3322faf726e3d0e4b57dd9a28c5aee6c
Matteus-Barbosa <barbosamateus2004@gmail.com>
2024-06-03 10:16:02 -0300

    Code: Add MiniProjeto Contador praticando exceptions customizadas.

---
+++ src/Studies/MiniProjetos/Contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Studies.MiniProjetos.Contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
